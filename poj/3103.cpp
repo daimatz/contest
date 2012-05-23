5f14b459f059bb08c75ac95a5089f0c5c8645a2d
dai <dai@daimatz.net>
2012-05-23 18:45:32 +0900

    poj 3103
    (0,0,0), (x,y,z) を対角の頂点とする直方体を n 等分する問題。
    サンプルは難しく等分しているけど、どのように等分してもいいっぽいので
    当然薄くスライスしても通るだろーと思って投げた。
    G++では通らないがC++では通るという現象に見舞われた。

---
+++ poj/3103.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+#include <string>
+#include <cctype>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <complex>
+#include <set>
+#define DEBUG(x) cerr<<"line:"<<__LINE__<<", "<<#x" == "<<(x)<<endl;
+#define REP(i,n) for(int i=0;i<(int)(n);++i)
+#define FOR(it,s) for(__typeof(s.begin()) it=s.begin();it!=s.end();++it)
+#define ALLOF(a) a.begin(),a.end()
+using namespace std;
+
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
+#define INF (INT_MAX/10-1)
+#define EPS (1.0e-8)
+
+int main(int argc, char *argv[]) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int x, y, z, n;
+  cin >> x >> y >> z >> n;
+
+  double d = (double)x/(double)n;
+  REP(i, n) {
+    printf("%.10lf %.10lf %.10lf %.10lf %.10lf %.10lf\n", d*i,0.0,0.0,d*(i+1),(double)y,(double)z);
+  }
+
+  return 0;
+}
