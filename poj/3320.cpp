2c6a2f826b73e5dca17fc92809c7f7f6354e568e
dai <dai@daimatz.net>
2012-06-20 01:09:44 +0900

    poj 3320
    教科書に書いてある内容を全部覚えたい。この教科書は同じ内容の事が何度も書
    かれており、全部は読まなくてもよい。連続したページ数を読むことにしたとき
    、最小のページ数を求めるという問題。
    
    蟻本。しゃくとり法的な感じでやります。

--- poj/3320.cpp
+++ poj/3320.cpp
@@ -2,42 +2,61 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <list>
 #include <stack>
 #include <queue>
 #include <map>
-#include <stack>
 #include <string>
 #include <cctype>
 #include <cmath>
 #include <cstring>
 #include <climits>
 #include <complex>
-#define DEBUG(x) cout<<"line:"<<__LINE__<<", "<<#x" == "<<x<<endl;
-#define REP(i,n) for(int i = 0; i < (int)(n); i++)
-#define FOR(it,s) for(__typeof((s).begin()) it = s.begin(); it != s.end(); it++)
+#include <set>
+#define DEBUG(x) cerr<<"line:"<<__LINE__<<", "<<#x" == "<<(x)<<endl;
+#define REP(i,n) for(int i=0;i<(int)(n);++i)
+#define FOR(it,s) for(__typeof(s.begin()) it=s.begin();it!=s.end();++it)
+#define ALLOF(a) a.begin(),a.end()
 using namespace std;
 
-#define INF (INT_MAX/10-1)
-#define EPS 1.0e-8
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
 
-#define MAX_N 1000000
-bool prime[MAX_N+1];
+#define INF (INT_MAX/10-1)
+#define EPS (1.0e-8)
+
+int P;
+int V[1000000];
+
+void solve() {
+  set<int> all;
+  REP(i, P) all.insert(V[i]);
+  int n = all.size();
+
+  int s = 0, t = 0, num = 0;
+  map<int, int> count;
+  int res = P;
+  while (1) {
+    while (t < P && num < n)
+      if (count[V[t++]]++ == 0)
+        ++num;
+    if (num < n) break;
+    res = min(res, t-s);
+    if (--count[V[s++]] == 0)
+      --num;
+  }
+  printf("%d\n", res);
+}
 
 int main(int argc, char *argv[]) {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
 
-  prime[1] = false; prime[2] = true;
-  REP(n, MAX_N+1) {
-    if (prime[n])
-      for (int i = n*2; i < MAX_N+1; i += n)
-        prime[i] = false;
-  }
+  scanf("%d", &P);
+  REP(i, P) scanf("%d", V+i);
 
-  while (1) {
-    int a, d, n;
-    scanf("%d %d %d", &a, &d, &n);
-    if (a == 0 && d == 0 && n == 0)
-      break;
-  }
+  solve();
 
   return 0;
 }
