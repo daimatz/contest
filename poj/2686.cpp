334ecf413cf72a0996807a874fed2ef79e4e02fa
dai <dai@daimatz.net>
2012-06-24 00:03:38 +0900

    poj 2686
    N 個の都市があり、そのうちの A から B まで馬車で旅をする。 M 枚の馬車券
    にはそれぞれ馬の頭数が書いてあり、移動距離をその頭数で割った値が都市間の
    移動時間となる。1区間に使える馬車券は1枚のみで、1度使った馬車券は使えな
    い。この条件のもとで、最小の所要時間を求めるという問題。
    
    蟻本。馬車券の状態をビットで持って拡張ダイクストラする。AOJ 1138 では通
    るけど、POJではTLEする。

--- poj/2686.cpp
+++ poj/2686.cpp
@@ -2,10 +2,10 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <list>
 #include <stack>
 #include <queue>
 #include <map>
-#include <stack>
 #include <string>
 #include <cctype>
 #include <cmath>
@@ -13,68 +13,91 @@
 #include <climits>
 #include <complex>
 #include <set>
-#define DEBUG(x) cout<<"line:"<<__LINE__<<", "<<#x" == "<<x<<endl;
-#define REP(i,n) for(int i = 0; i < (int)(n); i++)
-#define FOR(it,s) for(__typeof((s).begin()) it = s.begin(); it != s.end(); it++)
-#define ALLOF(a) a.begin(), a.end()
+#define DEBUG(x) cerr<<"line:"<<__LINE__<<", "<<#x" == "<<(x)<<endl;
+#define REP(i,n) for(int i=0;i<(int)(n);++i)
+#define FOR(it,s) for(__typeof(s.begin()) it=s.begin();it!=s.end();++it)
+#define ALLOF(a) a.begin(),a.end()
 using namespace std;
 
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+
 #define INF (INT_MAX/10-1)
-#define EPS 1.0e-8
+#define EPS (1.0e-8)
 
-#define MAX_N 8
-#define MAX_M 30
-int n,m,a,b;
-int t[MAX_N];
-int d[MAX_M][MAX_M];
+struct Edge {
+  int to, cost;
+  Edge(int to, int cost) : to(to), cost(cost) {}
 
-double dp[1<<MAX_N][MAX_M];
+};
 
-void solve() {
-  for (int i = 0; i < 1<<n; i++)
-    for (int j = 0; j < m; j++)
-      dp[i][j] = INF;
+vector<Edge> Edges[30];
 
-  dp[(1<<n)-1][a-1] = 0;
-  double res = INF;
-  for (int S = (1<<n)-1; S >= 0; S--) {
-    res = min(res, dp[S][b-1]);
-    for (int v = 0; v < m; v++) {
-      for (int i = 0; i < n; i++) {
-        if (S & (1 << i)) {
-          for (int u = 0; u < m; u++) {
-            if (d[v][u] >= 0) {
-              dp[S & ~(1<<i)][u] = min(dp[S & ~(1<<i)][u], dp[S][v] + (double)d[v][u] / t[i]);
-            }
+int N, M, P, A, B;
+int ticket[8];
+//int edge[30][30];
+int S;
+double dist[30][1<<8];
+
+struct State {
+  int now;
+  double cost;
+  int ticket;
+  State(int now, double cost, int ticket) : now(now), cost(cost), ticket(ticket) {}
+  bool operator<(const State &a) const {
+    return cost > a.cost;
+  };
+};
+
+void dijkstra() {
+  REP(i, M) REP(j, 1<<8) dist[i][j] = INF;
+  priority_queue<State> q;
+  S = (1<<N)-1;
+  q.push(State(A, 0, S));
+  while (!q.empty()) {
+    const State p = q.top(); q.pop();
+    if (p.cost >= dist[p.now][p.ticket] - EPS) continue;
+    //cerr << "now: " << p.now << ", cost: " << p.cost << ", ticket: " << p.ticket << endl;
+    if (p.now == B) {
+      printf("%.5f\n", p.cost);
+      return;
+    }
+    dist[p.now][p.ticket] = p.cost;
+    REP(i, N) {
+      if ((1<<i) & p.ticket) {
+        FOR(j, Edges[p.now]) {
+          const int nticket = p.ticket & ~(1<<i);
+          const double ncost = p.cost + static_cast<double>(j->cost)/ticket[i];
+          if (dist[j->to][nticket] > ncost) {
+            q.push(State(j->to, ncost, nticket));
           }
         }
       }
     }
   }
-
-  if (res == INF)
-    printf("Impossible\n");
-  else
-    printf("%.3f\n", res);
+  puts("Impossible");
 }
 
 int main(int argc, char *argv[]) {
-  while (1) {
-    int p;
-    scanf("%d %d %d %d %d", &n, &m, &p, &a, &b);
-    if (m == 0)
-      break;
-    memset(d, -1, sizeof(d));
-    REP(i, n) {
-      scanf("%d", t+i);
-    }
-    REP(i, p) {
-      int x,y,z;
-      scanf("%d %d %d", &x, &y, &z);
-      d[x][y] = d[y][x] = z;
-    }
+  cin.tie(0);
+  ios::sync_with_stdio(false);
 
-    solve();
+  while (1) {
+    scanf("%d%d%d%d%d", &N, &M, &P, &A, &B);
+    if (N+M+P+A+B == 0) break;
+    --A; --B;
+    REP(i, N) scanf("%d", ticket+i);
+    REP(i, M) Edges[i].clear();
+    REP(i, P) {
+      int from, to, cost;
+      scanf("%d%d%d", &from, &to, &cost);
+      --from; --to;
+      Edges[from].push_back(Edge(to,cost));
+      Edges[to].push_back(Edge(from,cost));
+      //edge[from][to] = edge[to][from] = cost;
+    }
+    dijkstra();
   }
 
   return 0;
